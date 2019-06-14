66f5a59ada2958d8ce5619f4bc6498120599cbbd
Alex Covert <acovert@ucsc.edu>
2019-06-13 18:22:19 -0700

    add controls text & destroy by time script

---
+++ 163FinalProject/Assets/Alex - Smoke, Fog, Player/Scripts/DestroyByTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour
+{
+    [SerializeField] private float timeToDestroy;
+
+    private void Start()
+    {
+        Destroy(this.gameObject, timeToDestroy);
+    }
+}
